f6b2b16659ec574bcc75d3da03b12a48abf3013b
d-mindlin <mindlin@ucsb.com>
2020-06-13 12:02:47 -0700

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,10 @@
-require('dotenv').config();
-
+// import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+const sequelize = new Sequelize('ecommerca-backend', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3001,
+});
 
 module.exports = sequelize;
